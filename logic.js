6c4a7dbd54884d486ef45e628d4edd0e30f53fdb
Neider123 <neiderjoansesc@ufps.edu.co>
2022-09-07 21:05:11 -0500

    first commit

---
+++ logic.js
@@ -0,0 +1,16 @@
+function update(){
+   var time = new Date();
+   var deltaTime = 0;
+
+   if(document.readyState === "complete" || document.readyState === "interactive"){
+       setTimeout(Init,1);
+   }else{
+       document.addEventListener("DOMContentLoaded", Init);
+   }
+
+   function init(){
+       time =new Date();
+       start();
+       loop();
+   }
+}
